edcda25ade1fa236275f4bb6c936a19bc37a4165
Ucheadaeze <ucheigbokwe25@gmail.com>
2023-07-04 02:42:03 -0700

    DM

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,6 @@
-export default function getListStudentIds(array) {
-  if (!Array.isArray(array))
-    return [];
-  return array.map((value) => value.id);
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    return list.map((obj) => obj.id);
+  }
+  return [];
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,19 @@
 export default function cleanSet(set, startString) {
   const list = [];
-  if (typeof startString !== 'string' || startString === '') {
+
+if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
-  for (const x of set) {
-    if (typeof x === 'string' && x.startsWith(startString)) {
-      list.push(x.slice(startString.length));
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      list.push(item.slice(startString.length));
     }
   }
+
   return list.join('-');
+}
